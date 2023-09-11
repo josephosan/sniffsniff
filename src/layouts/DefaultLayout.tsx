0bcf96ea4f09f6c28b85899224951504af677652
joseph <95222288+josephosan@users.noreply.github.com>
2023-09-11 20:55:27 +0330

    base layout for desktop is done

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
-import {useApp} from "../store/app.store";
+import {Layout} from "antd";
+
+const {Sider, Content} = Layout;
 
 export const DefaultLayout: React.FC = () => {
-    const { isMobile } = useApp();
 
     return (
-        <div className={isMobile ? "" : "layout-container"}>
-            The default layout is here.
-            <Outlet />
-        </div>
+        <Layout hasSider className={"layout-container"}>
+            <Sider
+                style={{
+                    overflow: 'auto',
+                    height: '100%',
+                    position: 'relative',
+                    right: 0,
+                    top: 0,
+                    bottom: 0,
+                    borderRadius: '0 30px 30px 0'
+                }}
+            >
+                <h1 className={"text-primary"}> hello there</h1>
+            </Sider>
+            <Layout className="site-layout" style={{marginLeft: 200}}>
+                <Content style={{margin: '24px 16px 0', overflow: 'initial'}}>
+                    <Outlet/>
+                </Content>
+            </Layout>
+        </Layout>
     );
 }
