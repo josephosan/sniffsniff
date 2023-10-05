9b13c2e332022ebd7bf2d3ce68b96b678e17391c
joseph <95222288+josephosan@users.noreply.github.com>
2023-10-05 23:11:14 +0330

    added patch method, ready to merge with master

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -60,7 +60,7 @@ export default class ApiService {
         return await api.post(resource, config);
     }
 
-    public static patch() {
-        this.notify.showAlert('success', 'hello', 'message');
+    public static async patch(recourse: string, config: AxiosRequestConfig = {}): Promise<AxiosResponse> {
+        return await api.patch(recourse, config);
     }
 }
