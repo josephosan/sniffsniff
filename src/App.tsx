48936937c71d33a4c2a24d86245908c82d79d9d2
joseph <95222288+josephosan@users.noreply.github.com>
2023-10-05 22:43:51 +0330

    fixed dark mode on formBuilder

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,7 @@ function App() {
                 colorBgContainer: theme.cardBg,
                 colorTextDescription: theme.fadeTextColor,
                 colorTextPlaceholder: theme.fadeTextColor,
+                colorBgElevated: theme.cardBgLighter
             },
             Select: {
                 colorText: theme.fadeTextColor,
@@ -61,6 +62,12 @@ function App() {
                 colorBgContainer: theme.cardBg,
                 colorTextDescription: theme.fadeTextColor,
                 colorTextPlaceholder: theme.fadeTextColor,
+                optionSelectedBg: theme.mainBackgroundColor,
+                optionActiveBg: theme.mainBackgroundColor,
+                optionSelectedColor: theme.defaultTextColor,
+                selectorBg: theme.cardBg,
+                multipleItemBg: theme.cardBg,
+                colorBgElevated: theme.cardBgLighter
             },
             Divider: {
                 colorSplit: theme.primaryColor,
