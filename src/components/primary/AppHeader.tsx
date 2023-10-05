cefdb3995f3f33a2c5e9b912618d0c020df31028
Donya Rasouli <d4nyaras@gmail.com>
2023-10-06 00:10:28 +0330

    Refactor: icon-header-modal

--- src/components/primary/AppHeader.tsx
+++ src/components/primary/AppHeader.tsx
@@ -1,98 +1,63 @@
-import React, {useState} from "react";
-import {TopBarIconWrapper} from "../secondary/TopBarIconWrapper";
-import {Space} from "antd";
-import {useApp} from "../../store/app.store";
-import {darkConfig, lightConfig} from "../../config/app.config";
-import WrapperModal from "../secondary/WrapperModal";
-
+import React, { useState } from "react";
+import { TopBarIconWrapper } from "../secondary/TopBarIconWrapper";
+import { Space } from "antd";
+import { useApp } from "../../store/app.store";
+import { darkConfig, lightConfig } from "../../config/app.config";
+import IconHeaderModal from "./IconHeaderModal";
 interface AppHeaderProps {
-    isMobile: boolean;
-    sidebarClick: () => void;
+  isMobile: boolean;
+  sidebarClick: () => void;
 }
 
-export const AppHeader: React.FC<AppHeaderProps> = (
-    {
-        isMobile,
-        sidebarClick,
-    }
-) => {
-    const {theme, setThemeConfig} = useApp();
-    const [openModal, setOpenModal] = useState(false);
+export const AppHeader: React.FC<AppHeaderProps> = ({
+  isMobile,
+  sidebarClick,
+}) => {
+  const { theme, setThemeConfig } = useApp();
+  const [openModal, setOpenModal] = useState(false);
 
-    return (
-        <div
-            className={
-                "mt-3 w-100 d-flex align-items-center justify-content-" +
-                (isMobile ? "between" : "end")
-            }
-        >
-            {isMobile && (
-                <Space onClick={sidebarClick}>
-                    <TopBarIconWrapper iconClasses={"bi bi-list"}/>
-                </Space>
-            )}
-            <Space
-                onClick={() => {
-                    setOpenModal(true);
-                }}
-            >
-                {!isMobile ? (
-                    <>
-                        <TopBarIconWrapper
-                            iconClasses={"bi bi-" + (theme.mode === "dark" ? "moon" : "sun")}
-                            size={theme.mode === "dark" ? 20 : null}
-                            onClick={() =>
-                                setThemeConfig(theme === lightConfig ? darkConfig : lightConfig)
-                            }
-                        />
-                        <TopBarIconWrapper size={20} iconClasses={"bi bi-bell"}/>
-                        <TopBarIconWrapper iconClasses={"bi bi-person"}/>
-                    </>
-                ) : (
-                    <>
-                        <TopBarIconWrapper iconClasses={"bi bi-three-dots-vertical"}/>
+  return (
+    <div
+      className={
+        "mt-3 w-100 d-flex align-items-center justify-content-" +
+        (isMobile ? "between" : "end")
+      }
+    >
+      {isMobile && (
+        <Space onClick={sidebarClick}>
+          <TopBarIconWrapper iconClasses={"bi bi-list"} />
+        </Space>
+      )}
+      <Space
+        onClick={() => {
+          setOpenModal(true);
+        }}
+      >
+        {!isMobile ? (
+          <>
+            <TopBarIconWrapper
+              iconClasses={"bi bi-" + (theme.mode === "dark" ? "moon" : "sun")}
+              size={theme.mode === "dark" ? 20 : null}
+              onClick={() =>
+                setThemeConfig(theme === lightConfig ? darkConfig : lightConfig)
+              }
+            />
+            <TopBarIconWrapper size={20} iconClasses={"bi bi-bell"} />
+            <TopBarIconWrapper iconClasses={"bi bi-person"} />
+          </>
+        ) : (
+          <>
+            <TopBarIconWrapper iconClasses={"bi bi-three-dots-vertical"} />
 
-                        <WrapperModal
-                            elements={
-                                <div>
-                                    <div
-                                        className={"w-100"}
-                                        style={{
-                                            height: 50,
-                                        }}
-                                    ></div>
-                                    <div
-                                        className={"d-flex justify-content-between"}
-                                    >
-                                        <TopBarIconWrapper
-                                            size={20}
-                                            iconClasses={"bi bi-box-arrow-right"}
-                                        />
-                                        <TopBarIconWrapper size={20} iconClasses={"bi bi-gear"}/>
-                                        <TopBarIconWrapper
-                                            iconClasses={"bi bi-" + (theme.mode === "dark" ? "moon" : "sun")}
-                                            size={theme.mode === "dark" ? 20 : null}
-                                            onClick={() =>
-                                                setThemeConfig(theme === lightConfig ? darkConfig : lightConfig)
-                                            }
-                                        />
-                                        <TopBarIconWrapper size={20} iconClasses={"bi bi-bell"}/>
-                                        <TopBarIconWrapper
-                                            size={20}
-                                            iconClasses={"bi bi-person-check"}
-                                        />
-                                    </div>
-                                </div>
-                            }
-                            open={openModal}
-                            setOpenModal={setOpenModal}
-                            top={"9%"}
-                            footer={null}
-                            closable={false}
-                        ></WrapperModal>
-                    </>
-                )}
-            </Space>
-        </div>
-    );
+            <IconHeaderModal
+              theme={theme}
+              setThemeConfig={setThemeConfig}
+              openModal={openModal}
+              setOpenModal={setOpenModal}
+            />
+          </>
+        )}
+      </Space>
+    </div>
+  );
 };
