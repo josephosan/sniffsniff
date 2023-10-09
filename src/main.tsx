2ab7b0bf1f7e8d3fe8aa1b0a7c562bae2829a964
joseph <95222288+josephosan@users.noreply.github.com>
2023-10-09 21:34:43 +0330

    fixed font problem in notification

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,17 @@ import './styles/main.scss'
 import {AuthProvider} from "./store/auth.store";
 import {AppProvider} from "./store/app.store";
 import {NotifyProvider} from "./store/notify.store";
+import {ConfigProvider} from "antd";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthProvider>
         <AppProvider>
-            <NotifyProvider>
-                <App/>
-            </NotifyProvider>
+            <ConfigProvider theme={{token: {fontFamily: "IRAN-sans"}}}>
+                <NotifyProvider>
+                    <App/>
+                </NotifyProvider>
+            </ConfigProvider>
         </AppProvider>
     </AuthProvider>
 )
