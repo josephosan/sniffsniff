ecf8f6ea39d070e52d2d03d33bee9736072c1b80
danial <danmy7799@gmail.com>
2023-10-24 21:35:02 +0330

    add confirmpassword form in second mode

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -5,18 +5,38 @@ import {FormBuilderField} from "../@types/app";
 import {appConfig} from "../config/app.config";
 import {useNavigate} from "react-router-dom";
 import UnderlinedLink from "../components/secondary/UnderlinedLink";
+import { useNotify } from "../store/notify.store";
+import { Descriptions, message } from "antd";
+
 
 
 const ForgotPassword: React.FC = () => {
     const [mode, setMode] = useState<string>('email');
-    const {theme} = useApp();
+    const { theme } = useApp();
     const navigate = useNavigate();
+    const notifyStore = useNotify();
     const forgotPasswordFormFields: FormBuilderField[] = [
         {
             placeholder: 'ایمیل',
             name: 'email',
-            rules: [{required: true, message: 'فیلد ایمیل اجباری است!'}],
+            rules: [{ required: true, message: 'فیلد ایمیل اجباری است!' }],
             type: 'text'
+        }
+    ]
+    const confirmPasswordFormFields: FormBuilderField[] = [
+        {
+            placeholder: 'رمز عبور',
+            name: 'password',
+            rules: [{ required: true, message: 'فیلد رمز عبور اجباری است!' }],
+            type: 'password'
+
+        }
+       , {
+            placeholder: 'تایید رمز عبور',
+            name: 'confirmpassword',
+            rules: [{ required: true, message: 'فیلد تایید رمز عبور اجباری است!' }],
+            type: 'password'
+
         }
     ]
 
@@ -27,7 +47,16 @@ const ForgotPassword: React.FC = () => {
         // we set the mode to password and then render the Password and Confirm
         // password form.
         setMode(() => 'reset-password');
+    
     }
+    const handleCheckPassword = (passwordData: { password: string, passwordconfirm: string }) => {
+        if (passwordData.password != passwordData.passwordconfirm) {
+            
+            notifyStore.showMessage("info", "check your password", 3);
+        }
+    }
+
+
     return (
         <div className={"mx-3 d-flex flex-column justify-content-between align-items-center h-100"}
              style={{color: theme.fadeTextColor, fontSize: appConfig.smallFontSize}}>
@@ -52,9 +81,21 @@ const ForgotPassword: React.FC = () => {
                             colSM={24}
                         />
                     ) : (
-                        <>
-                            reset password form here.
-                        </>
+                         <div className={"w-100 mb-auto"}>
+                <FormBuilder
+                    fields={confirmPasswordFormFields}
+                    onFinish={handleCheckPassword}
+                    submitButtonFlex={"center"}
+                    submitButtonClasses={"px-5"}
+                    colXL={24}
+                    colSM={24}
+                    size={'large'}
+                    additionalElement={
+                        <div></div>
+                    }
+                    
+                />
+            </div>
                     )
                 }
             </div>
