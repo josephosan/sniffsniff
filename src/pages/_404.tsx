b26b5731b8a032ccf3fd8b5c9b1dc8b8986dbfef
joseph <95222288+josephosan@users.noreply.github.com>
2023-11-04 20:30:08 +0330

    some small changes on filter branch

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -2,17 +2,33 @@ import React from "react";
 import CustomImage from "../components/secondary/CustomImage";
 import {Button} from "antd";
 import {useNavigate} from "react-router-dom";
+import {useApp} from "../store/app.store";
 
 const _404: React.FC = () => {
+    const { theme } = useApp();
     const navigate = useNavigate();
 
+
     return (
-        <div style={{ fontFamily: 'IRAN-sans' }} className={"w-100 h-100 d-flex justify-content-center align-items-center flex-column"}>
+        <div
+            style={{
+                fontFamily: 'IRAN-sans',
+                backgroundColor: theme.mainBackgroundColor
+            }}
+            className={"w-100 h-100 d-flex justify-content-center align-items-center flex-column"}
+        >
             <CustomImage
                 src={"/src/assets/global/_404.svg"}
                 width={'80%'}
             />
-            <Button className={"mt-5"} onClick={() => navigate('/home')}>
+            <Button
+                className={"mt-5"}
+                onClick={() => navigate('/home')}
+                style={{
+                    backgroundColor: theme.cardBgLighter,
+                    color: theme.defaultTextColor
+                }}
+            >
                 بازگشت به خانه
             </Button>
         </div>
