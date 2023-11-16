7362cb85918cad93c9cfbe3861c95dbfc9e51f49
d4nya <d4nyaras@gmail.com>
2023-11-16 20:27:42 +0330

    Add wrapper glow

---
+++ src/components/secondary/WrapperGlow.tsx
@@ -0,0 +1,38 @@
+import { styled, keyframes } from 'styled-components';
+import { ReactElement } from 'react';
+
+interface GlowProps {
+    children: ReactElement;
+    time?: string;
+    delayTime?: string;
+    key?: string;
+    value1?: string;
+    value2?: string;
+}
+
+const WrapperGlow: React.FC<GlowProps> = ({
+    children,
+    time = '3s',
+    key = 'filter',
+    value1 = 'blur(5px)',
+    value2 = 'bluer(0)',
+    delayTime = '0',
+}) => {
+    const pulse = keyframes`
+    from {
+        ${key}: ${value1};
+    }
+    to {
+        ${key}: ${value2};
+    }
+  `;
+
+    const Bar = styled.div`
+        animation: ${pulse} ${time};
+        animation-delay: ${delayTime};
+    `;
+
+    return <Bar>{children}</Bar>;
+};
+
+export default WrapperGlow;
