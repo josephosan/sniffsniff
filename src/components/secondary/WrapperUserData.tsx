f95db412212ab04f7e2327cb9b92cbb699da66d4
joseph <95222288+josephosan@users.noreply.github.com>
2023-11-28 21:54:11 +0330

    implemented the WrapperUserData using the WrapperData component

--- src/components/secondary/WrapperUserData.tsx
+++ src/components/secondary/WrapperUserData.tsx
@@ -1,25 +1,56 @@
 import React from "react";
 import WrapperData from "./WrapperData";
 import WrapperUserImage from "../tiny/WrapperUserImage";
+import {Space} from "antd";
 import ActionIconWrapper from "./ActionIconWrapper";
 
 interface WrapperUserDataProps {
-    url: string
+    imageUrl: string,
+    actionIcons?: {
+        icon: string,
+        event: string
+    }[],
+    iconClicked?: (eventName: string) => void
 }
 
 const WrapperUserData: React.FC<WrapperUserDataProps> = (
     {
-        url
+        imageUrl,
+        actionIcons,
+        iconClicked
     }
 ) => {
-    const [clicked, setClicked] = useState('');
+    const handleActionIconClick = (event) => {
+        if (iconClicked) {
+            iconClicked(event);
+        }
+    }
+
     return (
-        <WrapperData>
-            <WrapperUserImage
-                url={url}
-            />
-            <ActionIconWrapper icon={url} iconClicked={() => setClicked("seen")} />
-            <ActionIconWrapper icon={url} iconClicked={() =>setClicked("remove")} />
+        <WrapperData
+            forceDesktop={true}
+            padding={"10px"}
+        >
+            <div className={"d-flex justify-content-between align-items-center"}>
+                <WrapperUserImage
+                    url={imageUrl}
+                    size={"40px"}
+                />
+
+                <Space className={"ms-2"}>
+                    {
+                        actionIcons && actionIcons.map(el => {
+                            return (
+                                <ActionIconWrapper
+                                    icon={el.icon}
+                                    iconClicked={() => handleActionIconClick(el.event)}
+                                    key={el.icon}
+                                />
+                            )
+                        })
+                    }
+                </Space>
+            </div>
         </WrapperData>
     );
 }
