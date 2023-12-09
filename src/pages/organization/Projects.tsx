47b2b9060c2c4b5b1c41be7400daf467fa9d75f1
joseph <95222288+josephosan@users.noreply.github.com>
2023-12-10 03:16:26 +0330

    implemented the bases of organization routes and pages

---
+++ src/pages/organization/Projects.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OrganizationProjects: React.FC = () => {
+    return (
+        <>
+            Organization Projects
+        </>
+    )
+}
+
+export default OrganizationProjects;
---
+++ src/pages/organization/View.tsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from "react";
+import {Outlet, useLocation, useNavigate, useParams} from "react-router-dom";
+import TabComponent from "../../components/primary/TabComponent";
+import WrapperCard from "../../components/secondary/WrapperCard";
+import {useApp} from "../../store/app.store";
+
+const OrganizationView: React.FC = () => {
+    const navigate = useNavigate();
+    const params = useParams();
+    const location = useLocation();
+    const {theme} = useApp();
+    const [activeTab, setActiveTab] = useState<string | null>(null);
+
+    useEffect(() => {
+        const tab = location.pathname.split('/')[location.pathname.split('/').length - 1];
+        setActiveTab(() => tab);
+    }, [location.pathname]);
+
+    const handleTabItemClick = (e) => {
+        navigate(`/organization/${params.id}/${e}`);
+    }
+
+    return (
+        <>
+            Organization View
+
+            <TabComponent
+                animation={{inkBar: true, tabPane: true}}
+                activeKey={activeTab}
+                onChange={handleTabItemClick}
+                items={
+                    [
+                        {
+                            key: 'projects',
+                            label: 'پروژه ها',
+                            children: <WrapperCard shadowed={false} backgroundColor={theme.cardBgLighter}><Outlet /></WrapperCard>
+                        },
+                        {
+                            key: 'settings',
+                            label: 'تنظیمات',
+                            children: <WrapperCard shadowed={false} backgroundColor={theme.cardBgLighter}><Outlet/></WrapperCard>
+                        }
+                    ]
+                }
+            />
+        </>
+    )
+}
+
+export default OrganizationView;
