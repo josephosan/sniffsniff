c071275921afd0f7108e0f2262102f64192096eb
d4nya <d4nyaras@gmail.com>
2023-12-11 21:12:28 +0330

    Add project + Change tab colors on focus

--- src/pages/organization/View.tsx
+++ src/pages/organization/View.tsx
@@ -1,50 +1,74 @@
-import React, {useEffect, useState} from "react";
-import {Outlet, useLocation, useNavigate, useParams} from "react-router-dom";
-import TabComponent from "../../components/primary/TabComponent";
-import WrapperCard from "../../components/secondary/WrapperCard";
-import {useApp} from "../../store/app.store";
+import React, { useEffect, useState } from 'react';
+import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
+import TabComponent from '../../components/primary/TabComponent';
+import WrapperCard from '../../components/secondary/WrapperCard';
+import { useApp } from '../../store/app.store';
+import { compileString } from 'sass';
 
 const OrganizationView: React.FC = () => {
     const navigate = useNavigate();
     const params = useParams();
     const location = useLocation();
-    const {theme} = useApp();
+    const { theme } = useApp();
     const [activeTab, setActiveTab] = useState<string | null>(null);
 
     useEffect(() => {
-        const tab = location.pathname.split('/')[location.pathname.split('/').length - 1];
+        const tab =
+            location.pathname.split('/')[
+                location.pathname.split('/').length - 1
+            ];
         setActiveTab(() => tab);
     }, [location.pathname]);
 
     const handleTabItemClick = (e) => {
+        console.log(e);
+
         navigate(`/organization/${params.id}/${e}`);
-    }
+    };
+
+    const handleonTabClick = () => {
+        console.log('heh');
+    };
 
     return (
-        <>
+        <div>
             Organization View
-
             <TabComponent
-                animation={{inkBar: true, tabPane: true}}
+                animation={{ inkBar: true, tabPane: true }}
                 activeKey={activeTab}
                 onChange={handleTabItemClick}
-                items={
-                    [
-                        {
-                            key: 'projects',
-                            label: 'پروژه ها',
-                            children: <WrapperCard shadowed={false} backgroundColor={theme.cardBgLighter}><Outlet /></WrapperCard>
-                        },
-                        {
-                            key: 'settings',
-                            label: 'تنظیمات',
-                            children: <WrapperCard shadowed={false} backgroundColor={theme.cardBgLighter}><Outlet/></WrapperCard>
-                        }
-                    ]
-                }
+                onTabClick={handleonTabClick}
+                items={[
+                    {
+                        key: 'projects',
+                        label: 'پروژه ها',
+                        children: (
+                            <WrapperCard
+                                shadowed={false}
+                                backgroundColor={theme.cardBgLighter}
+                                width="100%"
+                                height="70vh"
+                            >
+                                <Outlet />
+                            </WrapperCard>
+                        ),
+                    },
+                    {
+                        key: 'settings',
+                        label: 'تنظیمات',
+                        children: (
+                            <WrapperCard
+                                shadowed={false}
+                                backgroundColor={theme.cardBgLighter}
+                            >
+                                <Outlet />
+                            </WrapperCard>
+                        ),
+                    },
+                ]}
             />
-        </>
-    )
-}
+        </div>
+    );
+};
 
 export default OrganizationView;
