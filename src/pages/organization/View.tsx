4d2047f8ba6da4d4f6a9938c594381a99bf85f86
joseph <95222288+josephosan@users.noreply.github.com>
2023-12-12 02:12:11 +0330

    collapsable wrapper

--- src/pages/organization/View.tsx
+++ src/pages/organization/View.tsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Outlet, useLocation, useNavigate, useParams} from 'react-router-dom';
 import TabComponent from '../../components/primary/TabComponent';
 import WrapperCard from '../../components/secondary/WrapperCard';
-import { useApp } from '../../store/app.store';
+import {useApp} from '../../store/app.store';
+import CollapsableWrapper from "../../components/primary/CollapsableWrapper";
 
 const OrganizationView: React.FC = () => {
     const navigate = useNavigate();
     const params = useParams();
     const location = useLocation();
-    const { theme } = useApp();
+    const {theme} = useApp();
     const [activeTab, setActiveTab] = useState<string | null>(null);
 
     useEffect(() => {
         const tab =
             location.pathname.split('/')[
-                location.pathname.split('/').length - 1
-            ];
+            location.pathname.split('/').length - 1
+                ];
         setActiveTab(() => tab);
     }, [location.pathname]);
 
@@ -25,9 +26,22 @@ const OrganizationView: React.FC = () => {
 
     return (
         <div>
-            Organization View
+            <CollapsableWrapper
+                items={
+                    [
+                        {
+                            key: 'title',
+                            label: <div>hello</div>,
+                            children: <div>children</div>
+                        }
+                    ]
+                }
+                expandIconPosition={"end"}
+            />
+
+
             <TabComponent
-                animation={{ inkBar: true, tabPane: true }}
+                animation={{inkBar: true, tabPane: true}}
                 activeKey={activeTab}
                 onChange={handleTabItemClick}
                 items={[
@@ -41,7 +55,7 @@ const OrganizationView: React.FC = () => {
                                 width="100%"
                                 height="70vh"
                             >
-                                <Outlet />
+                                <Outlet/>
                             </WrapperCard>
                         ),
                     },
@@ -55,7 +69,7 @@ const OrganizationView: React.FC = () => {
                                 width={"100%"}
                                 height={"auto"}
                             >
-                                <Outlet />
+                                <Outlet/>
                             </WrapperCard>
                         ),
                     },
---
+++ src/services/OrganizationApiService.ts
@@ -0,0 +1,5 @@
+import ApiService from "./ApiService";
+
+export default class OrganizationApiService extends ApiService {
+    public static baseUrl = 'user/organization/';
+}
