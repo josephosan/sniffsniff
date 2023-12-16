2d45832f0d2788ddc602880baaa346459babc8a0
joseph <95222288+josephosan@users.noreply.github.com>
2023-12-17 01:35:00 +0330

    updated create pages

--- src/pages/organization/Create.tsx
+++ src/pages/organization/Create.tsx
@@ -16,14 +16,6 @@ const CreateOrganization: React.FC = () => {
             rules: [{required: true, message: 'وارد کردن نام اجباری است'}]
         },
         {
-            name: 'type',
-            type: 'text',
-            label: 'نوع',
-            placeholder: 'نوع',
-            required: true,
-            rules: [{required: true, message: 'وارد کردن نوع اجباری است'}]
-        },
-        {
             name: 'desc',
             type: 'textarea',
             label: 'توضیحات',
