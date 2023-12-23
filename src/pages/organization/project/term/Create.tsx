e7d627c350f432bd341bbd1211541844188d986c
danial <danmy7799@gmail.com>
2023-12-23 20:58:28 +0330

    add create term

--- src/pages/organization/project/term/Create.tsx
+++ src/pages/organization/project/term/Create.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { FormBuilderField } from "../../../../@types/app";
+import FormBuilder from "../../../../components/primary/FormBuilder";
 
 const CreateTerm: React.FC = () => {
+    const createTermFormFields: FormBuilderField[] = [
+        {
+            name: 'type',
+            type: 'select',
+           
+            placeholder: 'نوع',
+            options: [{ label: 'یادآوری', value: 'reminder' },
+                { label: 'تسک', value: 'task' },
+                { label: 'رویداد', value: 'event' },
+                
+            ]
+           
+           
+        }
+    ]
     return (
         <>
-            create term
+          <div className="d-flex flex-column justify-content-between align-items-center">
+            <div className="mt-5">
+                <FormBuilder
+                    fields={createTermFormFields}
+                    submitButtonFlex={"center"}
+                    colXL={96}
+                    colSM={96}
+                    fieldsPaddingLevel={"0"}
+                    submitButtonLabel={"ایجاد"}
+                />
+            </div>
+        </div>  
         </>
     );
 }
