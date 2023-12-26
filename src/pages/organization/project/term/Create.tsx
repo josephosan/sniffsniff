df64228e31fd96633ce886e055a9d959281490ef
joseph <95222288+josephosan@users.noreply.github.com>
2023-12-27 00:57:37 +0330

    please fix the issue

--- src/pages/organization/project/term/Create.tsx
+++ src/pages/organization/project/term/Create.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FormBuilderField } from "../../../../@types/app";
+import {FormBuilderField} from "../../../../@types/app";
 import FormBuilder from "../../../../components/primary/FormBuilder";
 
 const CreateTerm: React.FC = () => {
@@ -7,31 +7,28 @@ const CreateTerm: React.FC = () => {
         {
             name: 'type',
             type: 'select',
-           
             placeholder: 'نوع',
-            options: [{ label: 'یادآور', value: 'reminder' },
-                { label: 'تسک', value: 'task' },
-                { label: 'رویداد', value: 'event' },
-                
+            options: [{label: 'یادآور', value: 'reminder'},
+                {label: 'تسک', value: 'task'},
+                {label: 'رویداد', value: 'event'},
+
             ]
-           
-           
         }
     ]
     return (
         <>
-          <div className="d-flex flex-column justify-content-between align-items-center">
-            <div className="mt-5">
-                <FormBuilder
-                    fields={createTermFormFields}
-                    submitButtonFlex={"center"}
-                    colXL={96}
-                    colSM={96}
-                    fieldsPaddingLevel={"0"}
-                    submitButtonLabel={"ایجاد"}
-                />
+            <div className="d-flex flex-column justify-content-between align-items-center">
+                <div className="mt-5">
+                    <FormBuilder
+                        fields={createTermFormFields}
+                        submitButtonFlex={"center"}
+                        colXL={24}
+                        colSM={24}
+                        fieldsPaddingLevel={"0"}
+                        submitButtonLabel={"ایجاد"}
+                    />
+                </div>
             </div>
-        </div>  
         </>
     );
 }
