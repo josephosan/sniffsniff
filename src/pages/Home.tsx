fa7f410181ef398719dbf2b06a83de75d217e9d5
joseph <95222288+josephosan@users.noreply.github.com>
2023-12-28 01:55:14 +0330

    removed unnecessary items form sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import WrapperMessage from "../components/secondary/WrapperMessage";
-import SegmentedWrapper from "../components/secondary/SegmentedWrapper";
 
 
 export const Home: React.FC = () => {
     return (
         <>
-            <SegmentedWrapper
-                block={false}
-                options={
-                    [
-                        {
-                            label: 'hello',
-                            value: 1,
-                        },
-                        {
-                            label: 'name',
-                            value: 2,
-                        },
-                        {
-                            label: 'another',
-                            value: 3,
-                        },
-                    ]
-                }
-            />
+            dashboard
         </>
     );
 };
