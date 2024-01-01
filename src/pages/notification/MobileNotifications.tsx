3b94b715ff92680fdf749c72c1aaea2c34a09c14
d4nya <d4nyaras@gmail.com>
2024-01-01 23:46:53 +0330

    Connect notifications to server

--- src/pages/notification/MobileNotifications.tsx
+++ src/pages/notification/MobileNotifications.tsx
@@ -2,46 +2,72 @@ import { useState, useEffect } from 'react';
 import FormSkeletonLoading from '../../components/secondary/FormSkeletonLoading';
 import WrapperMessage from '../../components/secondary/WrapperMessage';
 import NoData from '../../components/tiny/NoData';
+import NotificationApiService from '../../services/NotificationApiService';
+import Loading from '../../components/secondary/Loading';
 
 const MobileNotifications = () => {
     const [mobileNotifications, setMobileNotifications] =
         useState<never[]>(null);
-    const [pageFirstLoading, setPageFirstLoading] = useState(false);
+    const [pageFirstLoading, setPageFirstLoading] = useState(true);
+    const [fetchMoreLoading, setFetchMoreLoading] = useState(false);
 
     useEffect(() => {
-        setMobileNotifications([
-            {
-                type: 'info',
-                title: 'tile',
-                desc: 'desc',
-            },
-            {
-                type: 'info',
-                title: 'tile',
-                desc: 'desc',
-            },
-        ]);
+        async function fetchData() {
+            await handleFetchMore();
+        }
+
+        fetchData();
     }, []);
+
+    const handleFetchMore = async () => {
+        if (mobileNotifications) setFetchMoreLoading(() => true);
+        else setPageFirstLoading(() => true);
+
+        try {
+            const res = await NotificationApiService.getAll();
+
+            setMobileNotifications((prevState) => {
+                if (prevState) return [...prevState, ...res.data.data.items];
+
+                return [...res.data.data.items];
+            });
+        } catch (e) {
+            console.log(e);
+        } finally {
+            if (mobileNotifications) setFetchMoreLoading(() => false);
+            else setPageFirstLoading(() => false);
+        }
+    };
+
     return (
         <>
-            <div className="d-flex flex-column gap-3">
+            <div className="d-flex flex-column gap-3 h-100  align-items-center">
                 {pageFirstLoading && (
                     <FormSkeletonLoading fillRow={true} count={10} />
                 )}
 
-                {mobileNotifications && mobileNotifications.length > 0 ? (
-                    mobileNotifications.map((notif, index) => {
-                        return (
-                            <WrapperMessage
-                                type={notif.type}
-                                title={notif.title}
-                                desc={notif.desc}
-                                key={index}
-                            />
-                        );
-                    })
-                ) : (
-                    <NoData />
+                {(mobileNotifications && mobileNotifications.length > 0) ||
+                fetchMoreLoading
+                    ? mobileNotifications.map((notif, index) => {
+                          return (
+                              <WrapperMessage
+                                  type={notif.type}
+                                  title={notif.title}
+                                  desc={notif.desc}
+                                  key={index}
+                              />
+                          );
+                      })
+                    : !pageFirstLoading && <NoData />}
+
+                {fetchMoreLoading && (
+                    <div
+                        className={
+                            'w-100 d-flex justify-content-center align-items-center'
+                        }
+                    >
+                        <Loading />
+                    </div>
                 )}
             </div>
         </>
