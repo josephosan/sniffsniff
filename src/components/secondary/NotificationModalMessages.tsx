18047d3315e76fea1ab8d5f3066f339b36dbeb05
joseph.o <josephosan1381@gmail.com>
2024-01-10 00:17:29 +0330

    fixed a small bug on notification modal

--- src/components/secondary/NotificationModalMessages.tsx
+++ src/components/secondary/NotificationModalMessages.tsx
@@ -1,13 +1,15 @@
-import React, {useEffect, useState} from "react";
-import WrapperMessage from "./WrapperMessage";
-import UnderlinedLink from "./UnderlinedLink";
-import NotificationApiService from "../../services/NotificationApiService";
-import NoData from "../tiny/NoData";
-import Loading from "./Loading";
+import React, { useEffect, useState } from 'react';
+import WrapperMessage from './WrapperMessage';
+import UnderlinedLink from './UnderlinedLink';
+import NotificationApiService from '../../services/NotificationApiService';
+import NoData from '../tiny/NoData';
+import Loading from './Loading';
+import { useNavigate } from 'react-router-dom';
 
 const NotificationModalMessages: React.FC = () => {
     const [loading, setLoading] = useState(true);
-    const [notifList, setNotifList] = useState<unknown[] | null>(null);
+    const [notifList, setNotifList] = useState<any[] | null>(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function getData() {
@@ -21,29 +23,47 @@ const NotificationModalMessages: React.FC = () => {
         setLoading(true);
 
         try {
-            const {data} = await NotificationApiService.getAll();
+            const { data } = await NotificationApiService.getAll();
             setNotifList(() => data.data.items);
         } catch (e) {
             console.log(e);
         } finally {
             setLoading(false);
         }
-    }
+    };
 
     return (
         <>
-            {
-                !loading && (notifList?.length === 0 || !notifList) ? (
-                    <NoData/>
-                ) : loading ? (
-                    <div className={"w-100 d-flex justify-content-center align-items-center p-2"}>
-                        <Loading/>
-                    </div>
-                ) : (
-                    <div className={"p-1 gap-1"}>
-                        {
-                            notifList && notifList.map(el => {
-                                return (
+            {!loading && (notifList?.length === 0 || !notifList) ? (
+                <NoData />
+            ) : loading ? (
+                <div
+                    className={
+                        'w-100 d-flex justify-content-center align-items-center p-2'
+                    }
+                >
+                    <Loading />
+                </div>
+            ) : (
+                <div className={'p-1 gap-1'}>
+                    {notifList &&
+                        notifList.map((el, index) => {
+                            return (
+                                <div
+                                    onClick={() =>
+                                        navigate(
+                                            `/notifications/${
+                                                el.type === 0
+                                                    ? 'public'
+                                                    : 'project'
+                                            }/${el.id}`,
+                                        )
+                                    }
+                                    style={{
+                                        cursor: 'pointer',
+                                    }}
+                                    key={index}
+                                >
                                     <WrapperMessage
                                         title={el.title}
                                         desc={el.description}
@@ -51,20 +71,17 @@ const NotificationModalMessages: React.FC = () => {
                                             el.type === 0 ? 'info' : 'warning'
                                         }
                                     />
-                                );
-                            })
-                        }
+                                </div>
+                            );
+                        })}
 
-                        <div className={"d-flex justify-content-center mt-3"}>
-                            <UnderlinedLink
-                                text={"بیشتر"}
-                            />
-                        </div>
-                    </div>
-                )
-            }
+                    {/* <div className={'d-flex justify-content-center mt-3'}>
+                        <UnderlinedLink text={'بیشتر'} />
+                    </div> */}
+                </div>
+            )}
         </>
     );
-}
+};
 
 export default NotificationModalMessages;
