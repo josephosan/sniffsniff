bdae9d756c11e9c659df1ca5af74ca99af78f5a9
d4nya <d4nyaras@gmail.com>
2024-02-07 23:06:40 +0330

    Implement tags box in profile settings

--- src/components/primary/TagWrapper.tsx
+++ src/components/primary/TagWrapper.tsx
@@ -1,38 +1,91 @@
-import React, { ReactNode } from 'react';
-import { Space, Tag } from 'antd';
+import React, { ReactNode, useState } from 'react';
+import { Space, Tag, Input } from 'antd';
 
-interface Tag {
-    color?: string;
-    closeIcon?: ReactNode;
-    icon?: ReactNode;
-    bordered?: boolean;
-    onClose?: (e: any) => void;
-    text?: string;
+interface TagItem {
+    text: string;
 }
 
 interface TagWrapperProps {
-    tagList: Tag[];
+    tagList: TagItem[];
+    onChange: (tagList: TagItem[]) => void;
+    removable?: boolean;
+    addable?: boolean;
+    bordered?: boolean;
+    style?: object;
 }
 
-const TagWrapper: React.FC<TagWrapperProps> = ({ tagList }) => {
-    return (
-        <Space>
-            {tagList &&
-                tagList.map((t) => {
-                    return (
-                        <Tag
-                            onClose={t.onClose}
-                            icon={t.icon}
-                            bordered={t.bordered}
-                            closeIcon={t.closeIcon}
-                            className="d-flex justify-content-center align-items-center mt-1 mb-0"
-                        >
-                            <div className="p-1">{t.text}</div>
-                        </Tag>
-                    );
-                })}
-        </Space>
-    );
-};
+const TagWrapper: React.FC<TagWrapperProps> = React.memo(
+    ({
+        tagList,
+        onChange,
+        removable = false,
+        addable = true,
+        bordered = true,
+        style,
+    }) => {
+        const [inputValue, setInputValue] = useState('');
+
+        const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            setInputValue(e.target.value);
+        };
+
+        const handleInputConfirm = () => {
+            if (inputValue.trim() !== '') {
+                const newTags = [...tagList, { text: inputValue.trim() }];
+                onChange(newTags);
+                setInputValue('');
+            }
+        };
+
+        const handleRemoveTag = (removedTag: TagItem) => {
+            const newTags = tagList.filter(
+                (tag) => tag.text !== removedTag.text,
+            );
+            onChange(newTags);
+        };
+
+        return (
+            <Space
+                className=" d-flex"
+                style={{
+                    flexWrap: 'wrap',
+                }}
+            >
+                {tagList &&
+                    tagList.map((tag, index) => {
+                        return (
+                            <Tag
+                                style={style}
+                                bordered={bordered}
+                                key={index}
+                                closable={removable}
+                                onClose={() => handleRemoveTag(tag)}
+                                className="d-flex justify-content-center align-items-center "
+                            >
+                                <div
+                                    className="p-1 "
+                                    style={{ color: 'white' }}
+                                >
+                                    {tag.text}
+                                </div>
+                            </Tag>
+                        );
+                    })}
+
+                {addable && (
+                    <Input
+                        style={{ width: '120px' }}
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        onBlur={handleInputConfirm}
+                        onPressEnter={handleInputConfirm}
+                        placeholder="تگ جدید"
+                        prefix={<i className="bi bi-plus"></i>}
+                    />
+                )}
+            </Space>
+        );
+    },
+);
 
 export default TagWrapper;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,16 +4,16 @@ import TagWrapper from '../components/primary/TagWrapper';
 export const Home: React.FC = () => {
     return (
         <>
-            <TagWrapper
-                tagList={[
-                    {
-                        text: 'hello',
-                        color: 'primary',
-                        closeIcon: <i className="bi bi-x-lg"></i>,
-                        onClose: (e: any) => console.log(e),
-                    },
-                ]}
-            />
+            {/* <TagWrapper
+            tagList={[
+                {
+                    text: 'hello',
+                    color: 'primary',
+                    closeIcon: <i className="bi bi-x-lg"></i>,
+                    onClose: (e: any) => console.log(e),
+                },
+            ]}
+            /> */}
         </>
     );
 };
